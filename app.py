070c66664fa2d57d396fe1a7b616cbdca8927769
mokev10 <mokev10@outlook.com>
2026-04-30 10:46:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,9 @@ except Exception:
 # --- Page config ---
 st.set_page_config(page_title="Générateur 2D-Doc CIN TD1", layout="wide"
                    page_icon="https://img.icons8.com/external-justicon-lineal-justicon/64/external-barcode-ecommerce-justicon-lineal-justicon.png",
-                  )
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 # --- CSS pour rendu proche de la première image (fond, carte, diviseur) ---
 st.markdown(
